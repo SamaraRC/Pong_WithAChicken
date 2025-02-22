9427ed7122d29be940a004443d0e5f7187bfc6f0
Samara Costello <samara.costello@gmail.com>
2025-02-22 17:12:12 +1030

    Unity Setup + Project

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject Player1Racket;
+    public GameObject Player1Goal;
+
+    [Header("Player 2")]
+    public GameObject Player2Racket;
+    public GameObject Player2Goal;
+
+    [Header("Score UI")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+
+    private int Player1Score;
+    private int Player2Score;
+
+    public void Player1Scored()
+    {
+        Player1Score++;
+        Player1Text.GetComponent<TextMeshProUGUI>().text = Player1Score.ToString();
+        ResetPosition();
+    }
+
+    public void Player2Scored()
+    {
+        Player2Score++;
+        Player2Text.GetComponent<TextMeshProUGUI>().text = Player2Score.ToString();
+        ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        Player1Racket.GetComponent<Racket>().Reset();
+        Player2Racket.GetComponent<Racket>().Reset();
+    }
+}
+
+
